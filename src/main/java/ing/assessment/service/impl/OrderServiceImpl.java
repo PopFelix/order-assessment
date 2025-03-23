1ab5e40e8dd77f0583be30c99a173d106ced634f
PopFelix <felcris2001@gmail.com>
2025-03-23 22:51:36 +0200

    Added cost and delivery calculation logic for orders

--- src/main/java/ing/assessment/service/impl/OrderServiceImpl.java
+++ src/main/java/ing/assessment/service/impl/OrderServiceImpl.java
@@ -1,13 +1,18 @@
 package ing.assessment.service.impl;
 
 import ing.assessment.controller.dto.CreateOrderRequestDto;
+import ing.assessment.controller.dto.OrderProductDto;
 import ing.assessment.db.order.Order;
+import ing.assessment.db.product.Product;
 import ing.assessment.db.repository.OrderRepository;
+import ing.assessment.db.repository.ProductRepository;
+import ing.assessment.exception.NoProductFoundException;
 import ing.assessment.service.OrderService;
 import ing.assessment.service.validator.OrderProductValidator;
 import ing.assessment.util.OrderProductConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.List;
@@ -17,6 +22,7 @@ import java.util.List;
 public class OrderServiceImpl implements OrderService {
     private final OrderRepository orderRepository;
     private final OrderProductValidator orderProductValidator;
+    private final ProductRepository productRepository;
 
     @Override
     public List<Order> getAllOrders() {
@@ -24,12 +30,25 @@ public class OrderServiceImpl implements OrderService {
     }
 
     @Override
+    @Transactional
     public Order createOrder(CreateOrderRequestDto createOrderRequestDto) {
         Order order = new Order();
         orderProductValidator.validateProductStock(createOrderRequestDto.getOrderProducts());
         order.setOrderProducts(OrderProductConverter.convertToOrderProduct(createOrderRequestDto));
         order.setTimestamp(new Date());
-        order.setOrderCost(0.0);
+        order.setOrderCost(calculateOrderCost(createOrderRequestDto));
         return order;
+    }
+
+    private Double calculateOrderCost(CreateOrderRequestDto createOrderRequestDto) {
+        double orderCost = 0.0;
+        List<OrderProductDto> orderProducts = createOrderRequestDto.getOrderProducts();
+        for (OrderProductDto orderProduct : orderProducts) {
+            Product product = productRepository.findByProductCk_Id(orderProduct.getProductId()).stream()
+                    .findFirst()
+                    .orElseThrow(() -> new NoProductFoundException("No product found for id: " + orderProduct.getProductId()));
+            orderCost += product.getPrice() * orderProduct.getQuantity();
+        }
+        return orderCost;
     }
 }
--- src/main/java/ing/assessment/util/OrderCalculatorUtil.java
+++ src/main/java/ing/assessment/util/OrderCalculatorUtil.java
@@ -1,8 +1,16 @@
 package ing.assessment.util;
 
+import ing.assessment.controller.dto.CreateOrderRequestDto;
+import ing.assessment.db.product.Product;
 import lombok.experimental.UtilityClass;
+
+import java.util.List;
 
 @UtilityClass
 public class OrderCalculatorUtil {
 
+    public static Double calculateOrderCost(List<Product> products, CreateOrderRequestDto createOrderRequestDto) {
+
+        return null;
+    }
 }
